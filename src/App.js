4dc2fa5d698384026b3465f31765d29621e65690
JustasBac <justas.bac@outlook.com>
2021-04-17 15:42:49 +0200

    pridejau pages ir stiliaus

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Home from './components/pages/Home'
 
 function App() {
   return (
@@ -8,9 +10,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact />
+          <Route path='/' exact component={Home} />
         </Switch>
       </Router>
+
 
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from './Button';
 import './Navbar.css';
@@ -18,13 +18,17 @@ function Navbar() {
         }
     }
 
+    useEffect(() => { // shows button tik viena karta!
+        showButton();
+    }, []);
+
     window.addEventListener('resize', showButton)
 
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                         TRVL <i className="fas fa-plane-departure"></i>
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
