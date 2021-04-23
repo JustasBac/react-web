299f0a72a8bb848be24a49ff055ee99d4eb179de
JustasBac <justas.bac@outlook.com>
2021-04-23 08:04:21 +0200

    photo istrintos, linkai atnaujinti, jpeg prideta

--- src/components/pages/Deals.js
+++ src/components/pages/Deals.js
@@ -4,7 +4,6 @@ import { motion } from 'framer-motion'
 import { pageVariants } from '../../App.js'
 import { pageTransitions } from '../../App.js'
 import CardItem from '../CardItem'
-import picture1 from '../../images/img-9.jpg'
 
 export default function Deals() {
     return (
@@ -13,7 +12,7 @@ export default function Deals() {
                 <h1>SPECIAL DEALS</h1>
                 <ul className="listas">
                     <CardItem
-                        src={picture1}
+                        src='https://res.cloudinary.com/dfx4mjv8p/image/upload/c_scale,w_540/v1619156950/react-app/img-9_q1ymgn.jpg'
                         lokacija="Lithuania"
                         text="Discover the colours you never knew existed"
                         label='Sightseeing & Nature'
